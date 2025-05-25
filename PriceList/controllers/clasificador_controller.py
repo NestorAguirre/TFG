236dfd0e19a4a7b77e77392bb01d249a0a24d93f
Néstor <71756623+NestorAguirre@users.noreply.github.com>
2025-05-25 16:50:49 +0200

    PopUp de kivymd a kivy para evitar crasheo
    En algunos dispositivos android algunas funciones de kivymd no son compatibles y hacen que pete la aplicación

--- PriceList/controllers/clasificador_controller.py
+++ PriceList/controllers/clasificador_controller.py
@@ -1,19 +1,17 @@
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.spinner import Spinner
 from kivy.clock import Clock
-from kivy.lang import Builder
-from kivy.metrics import dp
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.menu import MDDropdownMenu
-from kivy.logger import Logger
 from kivy.utils import platform
-from kivy.factory import Factory
-from kivymd.uix.list import OneLineListItem
+from kivy.logger import Logger
+
+from functools import partial
 
 from modules.listados import productos_por_familia, FAMILIAS_FIJAS
 from controllers.dbcontroller import DBController
 from controllers.utils import get_db_path
-
-Builder.load_file("views/clasificador_popup.kv")
 
 
 class ClasificadorPopup:
@@ -22,62 +20,67 @@ class ClasificadorPopup:
         self.producto = producto
         self.precio = precio
         self.callback = callback
-        self.dialog = None
-        self.menu = None
         self.familia_seleccionada = None
-        self.button_select = None
-        self.label = None
+        self.popup = None
+        self.spinner = None
 
     def mostrar(self):
-        self.app.clasificador_popup = self
+        layout = BoxLayout(orientation='vertical', spacing=15, padding=20)
 
-        layout = Factory.ClasificadorPopupLayout()
-        self.label = layout.ids.producto_label
-        self.label.text = f"Producto: {self.producto}"
-        self.button_select = layout.ids.button_select
-
-        self.dialog = MDDialog(
-            title=f"Asignar familia a '{self.producto}'",
-            type="custom",
-            content_cls=layout,
-            buttons=[
-                MDFlatButton(text="CANCELAR", on_release=self.cerrar),
-                MDFlatButton(text="GUARDAR", on_release=self.guardar)
-            ]
+        label = Label(
+            text=f"Producto: {self.producto}",
+            size_hint=(1, 0.2),
+            font_size=18,
+            halign='center',
+            valign='middle'
         )
-        self.dialog.scrim_color = [0, 0, 0, 0]
-        self.dialog.elevation = 0
-        self.dialog.radius = [0, 0, 0, 0]
-        self.dialog.background_color = [0, 0, 0, 0]
-        self.dialog.open()
 
-    def abrir_menu(self, instance):
-        if not self.menu:
-            self.menu = MDDropdownMenu(
-                caller=self.button_select,
-                items=[
-                    {
-                        "viewclass": "OneLineListItem",
-                        "text": familia,
-                        "on_release": lambda x=familia: self.seleccionar_familia(x),
-                        "theme_text_color": "Custom",
-                        "text_color": [0.1, 0.1, 0.1, 1],
-                        "bg_color": [0.9, 0.9, 0.9, 1],
-                    }
-                    for familia in FAMILIAS_FIJAS
-                ],
-                width_mult=3,
-                background_color=[0, 0, 0, 0],
-                max_height=dp(200),
-                elevation=0
-            )
-        self.button_select.parent.do_layout()
-        Clock.schedule_once(lambda dt: self.menu.open(), 0.3)
+        layout.add_widget(label)
 
-    def seleccionar_familia(self, familia):
-        self.familia_seleccionada = familia
-        self.button_select.text = familia
-        self.menu.dismiss()
+        self.spinner = Spinner(
+            text="Selecciona familia",
+            values=FAMILIAS_FIJAS,
+            size_hint=(1, 0.2),
+            background_color=(0.9, 0.9, 1, 1)
+        )
+        self.spinner.bind(text=self.seleccionar_familia)
+
+        layout.add_widget(self.spinner)
+
+        botones = BoxLayout(size_hint=(1, 0.3), spacing=15)
+
+        btn_cancelar = Button(
+            text="Cancelar",
+            background_color=(0.6, 0, 0, 1),
+            color=(1, 1, 1, 1),
+            font_size=16,
+            on_release=partial(self.cerrar)
+        )
+
+        btn_guardar = Button(
+            text="Guardar",
+            background_color=(0, 0.6, 0, 1),
+            color=(1, 1, 1, 1),
+            font_size=16,
+            on_release=partial(self.guardar)
+        )
+
+
+        botones.add_widget(btn_cancelar)
+        botones.add_widget(btn_guardar)
+        layout.add_widget(botones)
+
+        self.popup = Popup(
+            title=f"Asignar familia a '{self.producto}'",
+            content=layout,
+            size_hint=(0.9, 0.5),
+            auto_dismiss=False
+        )
+        self.popup.open()
+
+    def seleccionar_familia(self, spinner, value):
+        self.familia_seleccionada = value
+        Logger.info(f"Familia seleccionada: {value}")
 
     def guardar(self, *args):
         if not self.familia_seleccionada:
@@ -90,17 +93,18 @@ class ClasificadorPopup:
             db = DBController("data/pricelist.db")
         else:
             db = DBController(get_db_path())
+
         db.insertarProducto(self.producto, self.familia_seleccionada)
         db.insertarPrecio(db.getProductoPorNombre(self.producto), db.getUltimoTicket(), self.precio)
 
-        Logger.info(f"ClasificadorPopup: '{self.producto}' asignado a familia '{self.familia_seleccionada}'")
+        Logger.info(f"'{self.producto}' asignado a familia '{self.familia_seleccionada}'")
         self.cerrar()
 
     def cerrar(self, *args):
-        if self.dialog:
-            self.dialog.dismiss()
-            self.dialog.bind(on_dismiss=self._al_cerrar_dialogo)
+        if self.popup:
+            self.popup.dismiss()
+            Clock.schedule_once(lambda dt: self._al_cerrar_popup(), 0.1)
 
-    def _al_cerrar_dialogo(self, *args):
+    def _al_cerrar_popup(self):
         if self.callback:
-            Clock.schedule_once(lambda dt: self.callback(self.app), 0.2)
+            self.callback(self.app)
